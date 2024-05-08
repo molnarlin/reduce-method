2bf2f430ab4eccce524d0a3b9feb9d7414f6fb77
Katalin Molnar <74814196+molnarlin@users.noreply.github.com>
2024-05-08 13:02:14 +0100

    methods added

--- reduce.js
+++ reduce.js
@@ -6,7 +6,17 @@
 
 
 // Summing an array of numbers:
-
+const nums = [0,1,2,3,4];
+// let sum = nums.reduce((acc, curr) => acc + curr, 0);
+let sum = nums.reduce((acc, curr) => {
+  console.log(
+    "Accumulator:", acc,
+    "Current value:", curr,
+    "Total:", acc + curr,
+  ); 
+  return acc + curr;
+}, 10);
+console.log(sum);
 
 const teamMembers = [
   {
