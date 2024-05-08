ebe126fd4180dfbd4d380c059f0aec72db7830b6
Katalin Molnar <74814196+molnarlin@users.noreply.github.com>
2024-05-08 13:36:43 +0100

    added more methods

--- reduce.js
+++ reduce.js
@@ -42,6 +42,18 @@ const teamMembers = [
 ];
 
 // Totaling a specific object property
-
+const totalExperience = teamMembers.reduce((acc, curr) => acc + curr.yrsExperience, 0);
+console.log(totalExperience);
 
 // Grouping by a property, and totaling it too
+let experienceByProfession = teamMembers.reduce((acc, curr) => {
+  let key = curr.profession;
+  if (!acc[key]){
+    acc[key] = curr.yrsExperience;
+  } else{
+    acc[key] += curr.yrsExperience;
+  }
+  return acc;
+}, {});
+
+console.log(experienceByProfession);
